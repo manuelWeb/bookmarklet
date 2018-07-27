d9f41a328fd66e63368acc8b549d87cba7a4ad21
macfly <manuel@webdesignord.fr>
2018-07-27 16:18:20 +0200

    var to const if possible or let otherwise

--- ref_tab_filter.js
+++ ref_tab_filter.js
@@ -1,27 +1,22 @@
 /*==========================================
 =            get ref in INFO.md            =
 ==========================================*/
+const ref = document.querySelectorAll('tr > td')
+let idx = 0
+
 // get REF
-var ref = document.querySelectorAll('tr > td')
-var lref = ref.length
-var idx = 0
-var result = []
 ref.forEach(elt => {
   if(idx % 4 === 0){
-    result.push( elt.textContent )
     document.getElementById('_html').innerHTML += '<span>'+elt.textContent+'<br /></span>'
   }
   idx++
 })
 
 // get LIB
-var ref = document.querySelectorAll('tr > td')
-// var lib = function(x){ return (4*x)+1}
-var lib = (x) => (4*x)+1
-var lib_max = 0
+const lib = (x) => (4*x)+1
+let lib_min = 0
 
-while(lib_max < 4){
-  // console.log(ref[lib(lib_max)])
-  document.getElementById('_html').innerHTML += '<span>'+ref[lib(lib_max)].textContent+'<br /></span>'
-  lib_max ++
+while(lib_min < 4){
+  document.getElementById('_html').innerHTML += '<span>'+ref[lib(lib_min)].textContent+'<br /></span>'
+  lib_min ++
 }
